bb0eafb3679cdc132d34c73d849df653c4716a36
NoeSekigami <141192154+sekigaminoe@users.noreply.github.com>
2023-12-06 09:43:39 +0900

    Chapter08_2-switch文練習

---
+++ src/Chapter08_2.java
@@ -0,0 +1,33 @@
+
+public class Chapter08_2 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		String week = "Sunday";
+		switch (week) {
+		case "Sunday":
+			System.out.println("日曜日です");
+			break;
+		case "Monday":
+			System.out.println("月曜日です");
+			break;
+		case "Tuesday":
+			System.out.println("火曜日です");
+			break;
+		case "Wednesday":
+			System.out.println("水曜日です");
+			break;
+		case "Thursday":
+			System.out.println("木曜日です");
+			break;
+		case "Friday":
+			System.out.println("金曜日です");
+			break;
+		case "Saturday":
+			System.out.println("土曜日です");
+			break;
+		default:
+			System.out.println("曜日ではありません");
+		}		
+	}
+}
